4831dfa6d094ddcddeeb14cb784b37cfaf72362c
muhammad-yasir <yasir.muhammad1983@gmail.com>
2022-05-02 12:16:34 +1200

    Update streamlit_app.py
    Added Menu in the list

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,5 @@ import streamlit
 streamlit.title('My Mom\'s new Healthy Dinner')
 
 streamlit.header('Breakfast Menu')
+
+streamlit.text('Omega 3 & Blueberry Oatmeal\n Kale, Spinach & Rocket Smoothie\n Hard-Boiled Free-Range Egg')
