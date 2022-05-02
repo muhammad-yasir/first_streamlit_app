e6ce0eb7fbe724e012319c20cec928cd7573b1ae
muhammad-yasir <yasir.muhammad1983@gmail.com>
2022-05-02 15:17:04 +1200

    Update streamlit_app.py
    Added more code

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,10 @@ streamlit.title('My Mom\'s new Healthy Dinner')
 
 streamlit.header('Breakfast Menu')
 
-streamlit.text('Omega 3 & Blueberry Oatmeal\n Kale, Spinach & Rocket Smoothie\n Hard-Boiled Free-Range Egg')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+
+steamlit.text('Kale, Spinach & Rocket Smoothie')
+
+steamlit.text('Hard-Boiled Free-Range Egg')
+
+steamlit.header('Build your own smoothie')
